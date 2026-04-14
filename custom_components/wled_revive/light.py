ebc2f13093836f1a21d65be2c018ebf3846a1d5b
AnasHost <41167157+Anashost@users.noreply.github.com>
2026-04-14 18:02:39 +0200

    Refactor WLED light entity management
    Refactor WLED light handling to support explicit segment IDs and improve command sending.

--- custom_components/wled_revive/light.py
+++ custom_components/wled_revive/light.py
@@ -22,10 +22,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             new_entities.append(WLEDMasterLight(data, config_entry.entry_id))
             known_segments.add("master")
             
-        for index, segment in enumerate(segs):
-            if index not in known_segments:
-                new_entities.append(WLEDSegmentLight(data, config_entry.entry_id, index))
-                known_segments.add(index)
+        # Parse based on explicit Segment ID, NOT array index!
+        for segment in segs:
+            seg_id = segment.get("id")
+            if seg_id is not None and seg_id not in known_segments:
+                new_entities.append(WLEDSegmentLight(data, config_entry.entry_id, seg_id))
+                known_segments.add(seg_id)
                 
         if new_entities:
             async_add_entities(new_entities)
@@ -46,6 +48,11 @@ class WLEDMasterLight(WledReviveEntity, LightEntity):
         self._attr_supported_features = LightEntityFeature.EFFECT
         self._attr_effect_list = list(self._data["effects_map"].keys())
 
+    def _get_target_segment(self):
+        """Grabs the first available segment to use as UI reference."""
+        segs = self.coordinator.data.get("state", {}).get("seg", [])
+        return segs[0] if segs else None
+
     @property
     def is_on(self):
         return self.coordinator.data["state"].get("on", False)
@@ -56,15 +63,17 @@ class WLEDMasterLight(WledReviveEntity, LightEntity):
 
     @property
     def rgb_color(self):
-        try: return tuple(self.coordinator.data["state"]["seg"][0]["col"][0][:3])
-        except (IndexError, KeyError): return (255, 255, 255)
+        seg = self._get_target_segment()
+        if seg and "col" in seg and len(seg["col"]) > 0:
+            return tuple(seg["col"][0][:3])
+        return (255, 255, 255)
 
     @property
     def effect(self):
-        try:
-            fx_id = self.coordinator.data["state"]["seg"][0]["fx"]
-            return self._data["id_to_effect"].get(fx_id, "Solid")
-        except (IndexError, KeyError): return "Solid"
+        seg = self._get_target_segment()
+        if seg and "fx" in seg:
+            return self._data["id_to_effect"].get(seg["fx"], "Solid")
+        return "Solid"
 
     async def _send_command(self, payload):
         try:
@@ -76,7 +85,10 @@ class WLEDMasterLight(WledReviveEntity, LightEntity):
 
     async def async_turn_on(self, **kwargs):
         payload = {"on": True}
-        seg_payload = {"id": 0}
+        
+        target_seg = self._get_target_segment()
+        seg_id = target_seg.get("id", 0) if target_seg else 0
+        seg_payload = {"id": seg_id}
         update_seg = False
         
         try: self.coordinator.data["state"]["on"] = True
@@ -91,16 +103,18 @@ class WLEDMasterLight(WledReviveEntity, LightEntity):
             rgb = kwargs[ATTR_RGB_COLOR]
             seg_payload["col"] = [[rgb[0], rgb[1], rgb[2]]]
             update_seg = True
-            try: self.coordinator.data["state"]["seg"][0]["col"][0] = [rgb[0], rgb[1], rgb[2]]
-            except (KeyError, IndexError): pass
+            if target_seg:
+                try: target_seg["col"][0] = [rgb[0], rgb[1], rgb[2]]
+                except (KeyError, IndexError): pass
                 
         if ATTR_EFFECT in kwargs:
             effect_id = self._data["effects_map"].get(kwargs[ATTR_EFFECT])
             if effect_id is not None:
                 seg_payload["fx"] = effect_id
                 update_seg = True
-                try: self.coordinator.data["state"]["seg"][0]["fx"] = effect_id
-                except (KeyError, IndexError): pass
+                if target_seg:
+                    try: target_seg["fx"] = effect_id
+                    except KeyError: pass
 
         if update_seg: payload["seg"] = [seg_payload]
         await self._send_command(payload)
@@ -124,32 +138,41 @@ class WLEDSegmentLight(WledReviveEntity, LightEntity):
         self._attr_supported_features = LightEntityFeature.EFFECT
         self._attr_effect_list = list(self._data["effects_map"].keys())
 
+    def _get_segment(self):
+        """Finds the segment dictionary matching this explicit ID."""
+        for seg in self.coordinator.data.get("state", {}).get("seg", []):
+            if seg.get("id") == self._segment_id:
+                return seg
+        return None
+
     @property
     def available(self) -> bool:
         if not self.coordinator.last_update_success: return False
-        return self._segment_id < len(self.coordinator.data.get("state", {}).get("seg", []))
+        return self._get_segment() is not None
 
     @property
     def is_on(self):
-        try: return self.coordinator.data["state"]["seg"][self._segment_id]["on"]
-        except (KeyError, IndexError): return False
+        seg = self._get_segment()
+        return seg.get("on", False) if seg else False
 
     @property
     def brightness(self):
-        try: return self.coordinator.data["state"]["seg"][self._segment_id]["bri"]
-        except (KeyError, IndexError): return 255
+        seg = self._get_segment()
+        return seg.get("bri", 255) if seg else 255
 
     @property
     def rgb_color(self):
-        try: return tuple(self.coordinator.data["state"]["seg"][self._segment_id]["col"][0][:3])
-        except (IndexError, KeyError): return (255, 255, 255)
+        seg = self._get_segment()
+        if seg and "col" in seg and len(seg["col"]) > 0:
+            return tuple(seg["col"][0][:3])
+        return (255, 255, 255)
 
     @property
     def effect(self):
-        try:
-            fx_id = self.coordinator.data["state"]["seg"][self._segment_id]["fx"]
-            return self._data["id_to_effect"].get(fx_id, "Solid")
-        except (IndexError, KeyError): return "Solid"
+        seg = self._get_segment()
+        if seg and "fx" in seg:
+            return self._data["id_to_effect"].get(seg["fx"], "Solid")
+        return "Solid"
 
     async def _send_command(self, payload):
         try:
@@ -161,30 +184,36 @@ class WLEDSegmentLight(WledReviveEntity, LightEntity):
 
     async def async_turn_on(self, **kwargs):
         seg_update = {"id": self._segment_id, "on": True}
-        try: self.coordinator.data["state"]["seg"][self._segment_id]["on"] = True
-        except KeyError: pass
+        seg = self._get_segment()
         
-        if ATTR_BRIGHTNESS in kwargs:
-            seg_update["bri"] = kwargs[ATTR_BRIGHTNESS]
-            try: self.coordinator.data["state"]["seg"][self._segment_id]["bri"] = kwargs[ATTR_BRIGHTNESS]
-            except KeyError: pass
-                
-        if ATTR_RGB_COLOR in kwargs:
-            rgb = kwargs[ATTR_RGB_COLOR]
-            seg_update["col"] = [[rgb[0], rgb[1], rgb[2]]]
-            try: self.coordinator.data["state"]["seg"][self._segment_id]["col"][0] = [rgb[0], rgb[1], rgb[2]]
-            except KeyError: pass
-                
-        if ATTR_EFFECT in kwargs:
-            effect_id = self._data["effects_map"].get(kwargs[ATTR_EFFECT])
-            if effect_id is not None:
-                seg_update["fx"] = effect_id
-                try: self.coordinator.data["state"]["seg"][self._segment_id]["fx"] = effect_id
+        if seg:
+            seg["on"] = True
+            
+            if ATTR_BRIGHTNESS in kwargs:
+                seg["bri"] = kwargs[ATTR_BRIGHTNESS]
+                seg_update["bri"] = kwargs[ATTR_BRIGHTNESS]
+                    
+            if ATTR_RGB_COLOR in kwargs:
+                rgb = kwargs[ATTR_RGB_COLOR]
+                seg_update["col"] = [[rgb[0], rgb[1], rgb[2]]]
+                try: seg["col"][0] = [rgb[0], rgb[1], rgb[2]]
                 except KeyError: pass
+                    
+            if ATTR_EFFECT in kwargs:
+                effect_id = self._data["effects_map"].get(kwargs[ATTR_EFFECT])
+                if effect_id is not None:
+                    seg_update["fx"] = effect_id
+                    seg["fx"] = effect_id
+        else:
+            # Fallback if segment isn't found but we need to push a command
+            if ATTR_BRIGHTNESS in kwargs: seg_update["bri"] = kwargs[ATTR_BRIGHTNESS]
+            if ATTR_EFFECT in kwargs:
+                eff_id = self._data["effects_map"].get(kwargs[ATTR_EFFECT])
+                if eff_id is not None: seg_update["fx"] = eff_id
 
         await self._send_command({"seg": [seg_update]})
 
     async def async_turn_off(self, **kwargs):
-        try: self.coordinator.data["state"]["seg"][self._segment_id]["on"] = False
-        except KeyError: pass
+        seg = self._get_segment()
+        if seg: seg["on"] = False
         await self._send_command({"seg": [{"id": self._segment_id, "on": False}]})
